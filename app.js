b851dc2b8648494486f29aea975901fda944189e
G Dong <johndong9317@gmail.com>
2016-08-05 04:21:43 +0000

    complete INDEX route and basic styling

--- app.js
+++ app.js
@@ -15,8 +15,27 @@ var BlogSchema = new mongoose.Schema({
    createdTime: {type: Date, default: Date.now()}
 });
 var Blog = mongoose.model("Blog", BlogSchema);
+Blog.create({
+    title: "My dog",
+    img: "https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Common-dog-behaviors-explained.jpg?itok=FSzwbBoi",
+    contents: "hello world"
+});
 //RESTful routes
 
+app.get("/", function(req, res) {
+    res.redirect("/blogs");
+})
+//INDEX route
+app.get("/blogs", function(req, res) {
+    //get all blogs from db and rende on index
+    Blog.find({}, function(err, allblogs) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", {blogs: allblogs}); 
+        }
+    })
+})
 //set up server listener
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("MyBlog server started...");
